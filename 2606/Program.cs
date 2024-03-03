0d66a3accade4b505b03e4057c222468996954ec
leemoojin <hzm_deli@naver.com>
2024-03-03 23:22:30 +0900

    2606 바이러스 깊이,너비

--- 2606/Program.cs
+++ 2606/Program.cs
@@ -6,6 +6,10 @@ namespace _2606
     internal class Program
     {        
         static int[,] connection;
+        static bool[] visited;
+        static int dfsCount = 0;//깊이우선탐색용
+       
+
         static void Main(string[] args)
         {   
             //enter the number of computers
@@ -24,17 +28,33 @@ namespace _2606
                 connection[input[0], input[1]] = 1;
                 connection[input[1], input[0]] = 1;
             }
-                       
-            Console.WriteLine(Bfs(input1));
+
+            //연결여부 탐색 확인용 -> 감염된 컴퓨터는 true
+            //1번 컴퓨터는 1번 인덱스에 넣는게 직관적이기 때문에 0번 인덱스를 스킵하기위 배열사이즈를 1 늘려야한다
+            visited = new bool[input1 + 1];
+            
+            
+            //너비 우선 탐색
+            Console.WriteLine(Bfs());
+
+
+            //깊이 우선 탐색
+            /*
+             * 깊이 우선탐색의 경우 (1,2) , (1,5), (2,3) 과 같이 감염되어 있을경우
+             * 1번과 2번이 감염 된것을 확인후 2번과 연결되어 감연된 컴퓨터를 찾기 위해 탐색한다
+             * 1,2에서 바이러스 발견 후 -> 2,3 를 찾아가는 함수가 실행 ~ 종료
+             * 아직 끝나지 않은 1,2를 탐색했던 함수가 이어서 1,5를 찾아간다
+             * 중간에 함수가 종료되기때문에 인자를 전달하는 방식으로는 
+             * 바이러스 갯수를 관리하는것이 불가능하므로 전역에서 관리한다
+             * */
+            //Dfs(1);
+            //Console.WriteLine(dfsCount);
+
         }
 
         //너비 우선 탐색
-        public static int Bfs(int input1)
-        {   
-            int computers = input1;//총 컴퓨터 수
-            //연결여부 탐색 확인용 -> 감염된 컴퓨터는 true
-            //1번 컴퓨터는 1번 인덱스에 넣는게 직관적이기 때문에 0번 인덱스를 스킵하기위 배열사이즈를 1 늘려야한다
-            bool[] visited = new bool[input1 + 1];
+        public static int Bfs()
+        {
             visited[1] = true;//1번 컴퓨터 감염으로 시작
             int count = 0;//1을 통해 감염된 컴퓨터 수
 
@@ -62,9 +82,23 @@ namespace _2606
         }
 
         //깊이 우선 탐색
-        public static void Dfs()
-        {
-        
+        public static void Dfs(int index)
+        {               
+            //이미 탐색한 컴퓨터면 종료
+            if (visited[index]) return;
+            
+            //탐색 처리
+            visited[index] = true;
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                if (connection[index, i] == 1 && !visited[i])
+                {
+                    dfsCount += 1;
+                    Dfs(i);
+                }
+            }
+
         }
     }
 }
