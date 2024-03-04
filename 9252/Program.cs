4c2f57fcccc7ca533b6efe02b5639293316fff81
leemoojin <hzm_deli@naver.com>
2024-03-04 23:59:54 +0900

    -

---
+++ 9252/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _9252
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+        }
+    }
+}
